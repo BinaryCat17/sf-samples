0fc3664a6a5eaf67c93713f574d4ace732d8364e
BinaryCat17 <smirnov.vladimir17@gmail.com>
2026-01-07 11:29:20 +0300

    fix

---
+++ vcpkg-ports/sf-samples/portfile.cmake
@@ -0,0 +1,16 @@
+set(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../sf-samples")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    GENERATOR "Unix Makefiles"
+)
+
+vcpkg_cmake_install()
+
+# Install tools to bin/ folder
+vcpkg_copy_tools(
+    TOOL_NAMES sf-runner sf-window
+    AUTO_CLEAN
+)
+
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/sf-samples/copyright" "Copyright (c) SionFlow")
